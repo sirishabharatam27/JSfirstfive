14183123202eaee42a453a7ae31190cb12fe6ba8
Sirisha Bhartam <sirishabharatam27@gmail.com>
2016-07-06 10:26:51 -0400

    The assignment program questions

---
+++ JS1-5/WebContent/a.js
@@ -0,0 +1,8 @@
+"use strict";
+var b;
+(function(){
+	  var a = b = 3;
+	})();
+
+	console.log("a defined? " + (typeof a !== 'undefined'));
+	console.log("b defined? " + (typeof b !== 'undefined'));
